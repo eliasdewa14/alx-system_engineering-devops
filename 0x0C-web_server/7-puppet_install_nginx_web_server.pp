af90fb3972d874c6f16f5f274d063e7ead1330f7
Elias-Dewa <elias.dewa19@gmail.com>
2023-01-28 08:09:50 -0800

    Bugs are fixed

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,13 +1,18 @@
 # Install nginx web server using puppet
 package { 'nginx':
-	ensure => installed,
-}
-file_line { 'install':
 	ensure => 'present',
+}
+execute { 'install':
+	command => 'sudo apt-get update; sudo apt-get -y install nginx',
+}
+execute { 'Hello':
+	command => 'echo "Hello World!" > /var/www/html/index.html'
+}
+file_line { 'sudo sed -i':
 	path   => '/etc/nginx/sites-available/default',
 	after  => 'listen 80 default_server;',
-	line   => 'rewrite ^/redirect_me https://www.youtube.com/channel/UC0ypST9g7cIDMjgGq9cgkJg permanent;',
+	line   => 'rewrite ^\/redirect_me return 301 Moved Permanently https:\/\/www.youtube.com\/@eliasd342 permanent;',
 }
-file { '/var/www/html/index.html':
-	content => 'Hello World!',
-} 
+execute { 'run':
+	command => 'sudo service nginx restart',
+}
