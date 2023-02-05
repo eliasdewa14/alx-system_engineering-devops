08e9b5c73eb8e117686e6e2094d53b7829b116e1
Elias-Dewa <elias.dewa19@gmail.com>
2023-02-05 02:11:45 -0800

    update 7-puppet_install

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,18 +4,18 @@ package { 'nginx':
 	ensure => 'present',
 }
 execute { 'install':
-	command => 'sudo apt-get update ; sudo apt-get -y install nginx',
+	command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
 	provider => shell,
 }
 execute { 'Hello':
-	command => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+	command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
 	provider => shell,
 }
 execute {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.youtube.com\/@eliasd342\/;\\n\\t}/" /etc/nginx/sites-available/default':
   provider => shell,
 }
 execute { 'run':
-	command => 'sudo service nginx restart',
+	command  => 'sudo service nginx restart',
 	provider => shell,
 }
 
