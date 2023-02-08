631952e0353df37d958bf8533931b868387ebe96
Elias-Dewa <elias.dewa19@gmail.com>
2023-02-08 15:09:05 -0800

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """The extend Python script to export data in the JSON format.
 """
+import json
 import requests
 from sys import argv
-import json
 
 
 if __name__ == '__main__':
