87e703765b3687efaeb5b9f34e422bc9dee50fe4
Elias-Dewa <elias.dewa19@gmail.com>
2023-02-08 14:57:59 -0800

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,13 +6,13 @@ from sys import argv
 
 
 if __name__ == '__main__':
-    user_url = 'https://jsonplaceholder.typicode.com/users/' + argv[1]
+    USER_ID = argv[1]
+    user_url = 'https://jsonplaceholder.typicode.com/users/' + USER_ID
 
     response = requests.get(user_url).json()
-    todos = user_url + "/todos"
-    tasks = requests.get(todos).json()
+    todo = user_url + "/todos"
+    tasks = requests.get(todo).json()
 
-    USER_ID = argv[1]
     USER_NAME = response.get('username')
 
     with open('{}.csv'.format(argv[1]), 'w') as file:
