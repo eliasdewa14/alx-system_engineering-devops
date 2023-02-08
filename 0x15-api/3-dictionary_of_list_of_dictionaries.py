59572504e3d04bd0ff1f670298bf46f25b0f18f3
Elias-Dewa <elias.dewa19@gmail.com>
2023-02-08 15:11:26 -0800

    Update 3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """The extend Python script to export data in the JSON format.
 """
+import json
 import requests
 from sys import argv
-import json
 
 
 if __name__ == '__main__':
@@ -14,8 +14,6 @@ if __name__ == '__main__':
         'https://jsonplaceholder.typicode.com/todos').json()
 
     dictionary = {}
-
-    url = "https://jsonplaceholder.typicode.com/users"
 
     dictionary = {}
     for user in users:
