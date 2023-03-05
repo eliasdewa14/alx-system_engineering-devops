98f5859e79ebbc2b869bb17fcc049c99da9cca05
Elias-Dewa <elias.dewa19@gmail.com>
2023-03-05 04:39:29 -0800

    web stack debugging using puppet

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fix the it when Wordpress is trying to initializes using puppet
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
